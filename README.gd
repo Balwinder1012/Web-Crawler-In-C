9d0a04acb050fccc56e580ee1ec7ac6b89406584
Balwinder Singh <balwinder1012@gmail.com>
2017-08-08 23:48:14 -0700

    add readme

---
+++ README.gd
@@ -0,0 +1,9 @@
+Web-Crawler-in-C
+
+It is a simple web crawler written in C language. It uses the wget command to make the http requests
+
+The program will run only in linux terminal, it will take three inputs as shown below
+
+$./crawler jmit.ac.in theDir 2
+
+jmit.ac.in is the seed url theDir is the directory in which html files will be saved 2 is the depth upto which the program will crawl
